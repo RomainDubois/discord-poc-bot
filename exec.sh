597c235707b50549308da30a107c3214a3f74361
Romain <no-need@deaf.com>
2026-09-01 21:20:51 +0200

    update cron

--- exec.sh
+++ exec.sh
@@ -1,6 +1,3 @@
-export DISCORD_IN_CHANNEL_ID=1544390320391716974
-export DISCORD_OUT_CHANNEL_ID=1544390251714445494
-
 msg=$(curl -s -H "Authorization: Bot $DISCORD_BOT_TOKEN"   "https://discord.com/api/v10/channels/$DISCORD_IN_CHANNEL_ID/messages?limit=1" | jq .[0].content -r)
 
 content="Hello depuis mon bot:
