cf60cf439654c36bd4a47160a9447c31b44d87e2
Romain <no-need@deaf.com>
2026-09-04 21:39:23 +0200

    Clean repo

--- exec.sh
+++ exec.sh
@@ -1,10 +1,13 @@
+# On récupère le dernier message d'un channel
 msg=$(curl -s -H "Authorization: Bot $DISCORD_BOT_TOKEN"   "https://discord.com/api/v10/channels/$DISCORD_IN_CHANNEL_ID/messages?limit=1" | jq .[0].content -r)
 
+# On travaille le message récupéré
 content="Hello depuis mon bot:
 ${msg}"
 
+# On envoie le message construit dans le channel cible
 jq -n \
-  --arg content "$1 - $content" \
+  --arg content "$content" \
   '{content: $content}' |
 curl -X POST \
   -H "Authorization: Bot $DISCORD_BOT_TOKEN" \
